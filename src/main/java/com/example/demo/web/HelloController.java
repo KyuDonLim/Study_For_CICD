f9f3e670843b66b4cd639a014531e9e03433cb81
임규돈 <mont4857@gmail.com>
2023-07-16 17:29:43 +0900

    push 17:30

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -13,7 +13,14 @@ public class HelloController {
     public String hello(@RequestParam(defaultValue = "1") Integer number){
         if(number == 1){ // info 로그
             log.info("/aws/v1 이 호출되었어요. info 로그 #####################################");
-            log.info("test 17:28");
+            log.info("test 17:30");
+            log.info("test 17:30");
+            log.info("test 17:30");
+            log.info("test 17:30");
+            log.info("test 17:30");
+            log.info("test 17:30");
+            log.info("test 17:30");
+
         }else if(number == -1){ // error 로그
             log.error("/aws/v1 이 호출되었어요. error 로그 #####################################");
         }else if(number == 0){ // warn 로그
